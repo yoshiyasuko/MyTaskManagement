df573c2f0b27784bb8357befd0206a9b1a0800e9
Yoshiyasu KO <te108128@gmail.com>
2024-02-10 00:51:59 +0900

    refactor: 💡 divided into composable: TaskListScreen
    #2

--- app/src/main/java/com/dashimaki_dofu/mytaskmanagement/view/activity/MainActivity.kt
+++ app/src/main/java/com/dashimaki_dofu/mytaskmanagement/view/activity/MainActivity.kt
@@ -4,26 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.dashimaki_dofu.mytaskmanagement.ui.theme.MyTaskManagementTheme
-import com.dashimaki_dofu.mytaskmanagement.view.composable.TaskList
+import com.dashimaki_dofu.mytaskmanagement.view.composable.screen.TaskListScreen
 import com.dashimaki_dofu.mytaskmanagement.viewModel.MainViewModel
 
 class MainActivity : ComponentActivity() {
     private val viewModel: MainViewModel by viewModels()
 
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen().apply {
@@ -34,28 +22,7 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             MyTaskManagementTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Scaffold(
-                        topBar = {
-                            TopAppBar(
-                                title = {
-                                    Text("課題")
-                                },
-                                colors = TopAppBarDefaults.topAppBarColors(
-                                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                                    titleContentColor = MaterialTheme.colorScheme.primary
-                                )
-                            )
-                        }
-                    ) { innerPadding ->
-                        Box(modifier = Modifier.padding(innerPadding)) {
-                            TaskList(viewModel.tasks)
-                        }
-                    }
-                }
+                TaskListScreen(tasks = viewModel.tasks)
             }
         }
     }
---
+++ app/src/main/java/com/dashimaki_dofu/mytaskmanagement/view/composable/screen/TaskListScreen.kt
@@ -0,0 +1,58 @@
+package com.dashimaki_dofu.mytaskmanagement.view.composable.screen
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.dashimaki_dofu.mytaskmanagement.model.Task
+import com.dashimaki_dofu.mytaskmanagement.model.makeDummyTasks
+import com.dashimaki_dofu.mytaskmanagement.view.composable.TaskList
+
+
+/**
+ * TaskListScreen
+ *
+ * Created by Yoshiyasu on 2024/02/10
+ */
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TaskListScreen(tasks: List<Task>) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = {
+                        Text("課題")
+                    },
+                    colors = TopAppBarDefaults.topAppBarColors(
+                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                        titleContentColor = MaterialTheme.colorScheme.primary
+                    )
+                )
+            }
+        ) { innerPadding ->
+            Box(modifier = Modifier.padding(innerPadding)) {
+                TaskList(tasks)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TaskListScreenPreview() {
+    TaskListScreen(tasks = makeDummyTasks())
+}
