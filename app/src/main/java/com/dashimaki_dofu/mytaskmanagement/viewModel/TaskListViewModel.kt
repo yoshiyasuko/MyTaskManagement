1fac6a10337dc0d1d629c3c934b6a2c5f9802863
Yoshiyasu KO <te108128@gmail.com>
2024-03-04 00:58:41 +0900

    refactor: 💡 UiStated - `TaskListScreen`

--- app/src/main/java/com/dashimaki_dofu/mytaskmanagement/viewModel/TaskListViewModel.kt
+++ app/src/main/java/com/dashimaki_dofu/mytaskmanagement/viewModel/TaskListViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.viewModelScope
 import com.dashimaki_dofu.mytaskmanagement.model.TaskSubject
 import com.dashimaki_dofu.mytaskmanagement.model.makeDummyTaskSubjects
 import com.dashimaki_dofu.mytaskmanagement.repository.TaskSubjectRepository
+import com.dashimaki_dofu.mytaskmanagement.viewModel.TaskListViewModel.UiState.Loaded
+import com.dashimaki_dofu.mytaskmanagement.viewModel.TaskListViewModel.UiState.Loading
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -20,7 +22,14 @@ import javax.inject.Inject
  */
 
 abstract class TaskListViewModel : ViewModel() {
-    abstract val taskSubjects: StateFlow<List<TaskSubject>>
+    sealed interface UiState {
+        data object Loading : UiState
+        data class Loaded(
+            val taskSubjects: List<TaskSubject>
+        ) : UiState
+    }
+
+    abstract val uiState: StateFlow<UiState>
 
     open fun fetchTaskSubjects() = Unit
 }
@@ -29,17 +38,18 @@ abstract class TaskListViewModel : ViewModel() {
 class TaskListViewModelImpl @Inject constructor(
     private val taskSubjectRepository: TaskSubjectRepository,
 ) : TaskListViewModel() {
-    private val _taskSubjects = MutableStateFlow<List<TaskSubject>>(emptyList())
-    override val taskSubjects = _taskSubjects.asStateFlow()
+    private val _uiState = MutableStateFlow<UiState>(Loading)
+    override val uiState = _uiState.asStateFlow()
 
     override fun fetchTaskSubjects() {
         viewModelScope.launch {
-            _taskSubjects.value = taskSubjectRepository.getAllTaskSubjects()
+            val taskSubjects = taskSubjectRepository.getAllTaskSubjects()
+            _uiState.value = Loaded(taskSubjects)
         }
     }
 }
 
 class TaskListViewModelMock : TaskListViewModel() {
-    override val taskSubjects: StateFlow<List<TaskSubject>>
-        get() = MutableStateFlow(makeDummyTaskSubjects())
+    override val uiState: StateFlow<UiState>
+        get() = MutableStateFlow(Loaded(makeDummyTaskSubjects()))
 }
