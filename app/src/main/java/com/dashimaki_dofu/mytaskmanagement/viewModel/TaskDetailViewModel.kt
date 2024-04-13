6737a00291902e974c56991342a66d6b8a3bb31b
Yoshiyasu KO <te108128@gmail.com>
2024-04-13 18:36:00 +0900

    feat: 🎸 implement copying task
    #126

--- app/src/main/java/com/dashimaki_dofu/mytaskmanagement/viewModel/TaskDetailViewModel.kt
+++ app/src/main/java/com/dashimaki_dofu/mytaskmanagement/viewModel/TaskDetailViewModel.kt
@@ -2,7 +2,10 @@ package com.dashimaki_dofu.mytaskmanagement.viewModel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.dashimaki_dofu.mytaskmanagement.model.SubTask
 import com.dashimaki_dofu.mytaskmanagement.model.SubTaskStatus
+import com.dashimaki_dofu.mytaskmanagement.model.SubTaskStatus.INCOMPLETE
+import com.dashimaki_dofu.mytaskmanagement.model.Task
 import com.dashimaki_dofu.mytaskmanagement.model.TaskSubject
 import com.dashimaki_dofu.mytaskmanagement.model.makeDummyTaskSubjects
 import com.dashimaki_dofu.mytaskmanagement.repository.TaskSubjectRepository
@@ -33,11 +36,15 @@ abstract class TaskDetailViewModel : ViewModel() {
 
     abstract val uiState: StateFlow<UiState>
     open val showDeleteAlertDialogState: StateFlow<Boolean> = MutableStateFlow(false)
+    open val showCopyAlertDialogState: StateFlow<Boolean> = MutableStateFlow(false)
 
     open fun fetchTaskSubject(taskId: Int) = Unit
     open fun showDeleteAlertDialog() = Unit
     open fun dismissDeleteAlertDialog() = Unit
+    open fun showCopyAlertDialog() = Unit
+    open fun dismissCopyAlertDialog() = Unit
     open fun deleteTask(taskId: Int, completion: () -> Unit) = Unit
+    open fun copyTask(taskSubject: TaskSubject, completion: () -> Unit) = Unit
     open fun updateSubTaskStatus(subTaskId: Int, status: SubTaskStatus) = Unit
 }
 //endregion
@@ -53,9 +60,32 @@ class TaskDetailViewModelImpl @Inject constructor(
     private val _showDeleteAlertDialogState = MutableStateFlow(false)
     override val showDeleteAlertDialogState = _showDeleteAlertDialogState.asStateFlow()
 
+    private val _showCopyAlertDialogState = MutableStateFlow(false)
+    override val showCopyAlertDialogState = _showCopyAlertDialogState.asStateFlow()
+
     override fun deleteTask(taskId: Int, completion: () -> Unit) {
         viewModelScope.launch {
             taskSubjectRepository.deleteTask(taskId)
+            completion.invoke()
+        }
+    }
+
+    override fun copyTask(taskSubject: TaskSubject, completion: () -> Unit) {
+        viewModelScope.launch {
+            val task = Task()
+            task.title = taskSubject.task.title
+            task.deadlineDate = taskSubject.task.deadlineDate
+            task.deadlineTime = taskSubject.task.deadlineTime
+
+            val taskId = taskSubjectRepository.saveTask(task)
+
+            taskSubject.subTasks.forEach {
+                val subTask = SubTask()
+                subTask.taskId = taskId
+                subTask.title = it.title
+                subTask.status = INCOMPLETE
+                taskSubjectRepository.saveSubTask(subTask)
+            }
             completion.invoke()
         }
     }
@@ -73,6 +103,14 @@ class TaskDetailViewModelImpl @Inject constructor(
 
     override fun dismissDeleteAlertDialog() {
         _showDeleteAlertDialogState.value = false
+    }
+
+    override fun showCopyAlertDialog() {
+        _showCopyAlertDialogState.value = true
+    }
+
+    override fun dismissCopyAlertDialog() {
+        _showCopyAlertDialogState.value = false
     }
 
     override fun updateSubTaskStatus(subTaskId: Int, status: SubTaskStatus) {
